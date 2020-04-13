24eb07c9ef0a5d3ec35bd67f3f5267f7fe315f19
Hassan Rezaei <hassanrezaei1400@gmail.com>
2020-04-13 18:46:21 +0430

    Add check phone number Function

--- script.js
+++ script.js
@@ -28,3 +28,21 @@ function checkEmail() {
     }
 }
 
+
+//  (2)
+function checkPhone() {
+    let value = document.getElementById("phone").value;
+    let result = false;
+    const Prefix = ["0910", "0919", "0990", "0930", "0933", "0935", "0936", "0937", "0938", "0939", "0901", "0902", "0903", "0920", "0921", "0931", "0932"];
+    let temp = value.substr(0, 4);
+    if (value.length === 11 && Prefix.includes(temp)) {
+        result = true;
+    }
+    if (result) {
+        document.getElementById("result-valid-phone").innerHTML = "Valid Phone Number";
+        document.getElementById("result-valid-phone").style.color = "#2dca4f";
+    } else {
+        document.getElementById("result-valid-phone").innerHTML = "Not Valid Phone Number";
+        document.getElementById("result-valid-phone").style.color = "#ff8d8d";
+    }
+}
